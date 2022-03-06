5460450519d6056e511049fd0d8064e6a6b9ec71
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-03-05 18:59:16 -0800

    Update calculator.js
    finish simple calculator

--- calculator.js
+++ calculator.js
@@ -3,9 +3,12 @@ function calculation(){
     //get the values from the prompt and store them in two vars
         let number1=prompt("Enter first number");
         let number2=prompt("Enter second number");
+        
 
     // do operation and store the results in vars (you need four vars)
         let sum=number1*number2;
         let add=number1+number2;
+        let sub=number1-number2;
+        let divide=number1/number2;
     //display the result in the HTML
-    document.getElementById("results").innerHTML=`<p>The sum is: ${add}</p>,<p>The sum is: ${sum}</p>`;
+    document.getElementById("results").innerHTML=`<p> ${number1}+${number2}= ${add}</p><p>${number1}*${number2}= ${sum}</p><p>${number1}-${number2}= ${sub}</p><p>${number1}/${number2}= ${divide}</p>`};
